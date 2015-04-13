798af64e0eb1020bf4a2949c406fbdfcb9b0f7fe
JuanYJaime <jaimeherrerias7@gmail.com>
2015-04-13 12:20:30 +0200

    test calendar

---
+++ TestCalendar/src/testCalendar/TestCalendar.java
@@ -0,0 +1,29 @@
+package testCalendar;
+
+import java.awt.Font;
+import java.awt.MenuItem;
+import java.awt.peer.MenuPeer;
+import java.util.Calendar;
+import java.util.Date;
+
+import utiles.Menu;
+
+public class TestCalendar {
+	static Date date  = new Date();
+	public static void main(String[] args) {
+		Menu menu = new Menu("Calendario", new String[] { "mostrar año",
+				"mostrar mes", "mostrar semana", "mostrar dia" });
+		realizarOpcion(menu.gestionarMenu());
+	}
+
+	private static void realizarOpcion(int opcion) {
+		// TODO Auto-generated method stub
+	
+	Calendar calendar = Calendar.getInstance();
+		switch(opcion){
+		case 1:
+			
+			System.out.println(calendar.getFirstDayOfWeek());
+		}
+	}
+}
