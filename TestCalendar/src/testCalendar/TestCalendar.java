6452d4e58eb638c61278a0582225a7d2782c223a
JuanYJaime <jaimeherrerias7@gmail.com>
2015-04-13 19:06:23 +0200

    -se ha completado el metodo realizarOpcion . -se ha cambiado el menu a las correspondientes opciones. -se ha aplicado un bucle do-while con el fin de repetir el metodo realizar opcion tanta veces como uno quiera. -se ha capturado la excepcion nullpointerexception con el fin de que cuando salte aparezca un mensaje -se ha almacenado el menu.gestionarMenu() en una variable opcion de tipo  entero

--- TestCalendar/src/testCalendar/TestCalendar.java
+++ TestCalendar/src/testCalendar/TestCalendar.java
@@ -7,24 +7,67 @@ import java.util.Calendar;
 import java.util.Date;
 
 import utiles.Menu;
+/**Crea el programa TestClaseCalendar con estos requisitos:
 
+*Ha de mostrar un menú con los siguientes elementos: Captura el instante, Año, Mes, Día, Día de la semana, Hora, Minutos, Segundos, Salir.
+*La primera opción crea una instancia de Calendar con el instante actual
+*Calendar.getInstance(); 
+*El resto de opciones (excepto Salir) extraen y muestran la información del objeto de tipo Calendar de dos formas distintas:
+*Mediante println():
+* System.out.println("Año con get: " + ahora.get(Calendar.YEAR)); 
+*Mediante format() utilizando todos los formatos posibles:
+* System.out.format("Año con format: %tC, %<tY, %<ty %n", ahora); 
+*En el caso en que se invoque a cualquiera de las opciones sin haber creado el objeto Calendar capturará la excepción NullPointerException y se mostrará el mensaje "Captura el instante antes de analizarlo." 
+**/
 public class TestCalendar {
-	static Date date  = new Date();
+	static Calendar calendar ;
 	public static void main(String[] args) {
-		Menu menu = new Menu("Calendario", new String[] { "mostrar año",
-				"mostrar mes", "mostrar semana", "mostrar dia" });
-		realizarOpcion(menu.gestionarMenu());
+		Menu menu = new Menu("Calendario", new String[] { "Capturar instante",
+				"Año", "mes", "dia","dia de la semana","hora","minutos","segundos","salir" });
+		int opcion;
+		do {
+			opcion = menu.gestionarMenu();
+			
+			try {
+				realizarOpcion(opcion);
+			} catch (Exception e) {
+				System.err.println("captura el instante antes de analizarlo\n");
+			}
+		} while (opcion != 9);
 	}
-
+/** este metodo realiza las opciones realizadas en el menu instanciado anteriormente 
+ * @param int opcion */
 	private static void realizarOpcion(int opcion) {
 		// TODO Auto-generated method stub
 	
-	Calendar calendar = Calendar.getInstance();
+	
 		switch(opcion){
 		case 1:
-			
-			System.out.println(calendar.getInstance());
-			System.out.println("adios");
+			calendar = Calendar.getInstance();
+			System.out.println("instante capturado");
+			break;
+		case 2:
+		
+			System.out.println(calendar.get(Calendar.YEAR));
+			break;
+		case 3:
+			System.out.println(calendar.get(Calendar.MONTH));
+			break;
+		case 4:
+			System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+			break;
+		case 5:
+			System.out.println(calendar.get(Calendar.DAY_OF_WEEK));
+			break;
+		case 6:
+			System.out.println(calendar.get(Calendar.HOUR));
+			break;
+		case 7:
+			System.out.println(calendar.get(Calendar.MINUTE));
+			break;
+		case 8:
+			System.out.println(calendar.get(Calendar.SECOND));
+			break;
 		}
 	}
 }
